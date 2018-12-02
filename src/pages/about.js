41b4a6de44886532e438d29ce317851a9397b13b
Endorel <helene@kumpan.se>
2018-12-02 11:42:48 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Header from '../components/header';
+
+export default () => <div style={{color: 'teal'}}>
+    <Header headerText="Hello!" />
+    <p>Such wow. Very react.</p>
+</div>
